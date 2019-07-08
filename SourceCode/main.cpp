009a7e5742a4f9ee245a5745aff948936ab7fb6f
Josh Gibson <josh.gibson@reflexgaming.com>
2019-07-08 13:33:54 +0100

    Added test code for testing number of Vulkan extentions and glfw window creation

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
 #include <glm/glm.hpp>
-
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 int main()
 {
-	
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t nVulkanExtentions = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &nVulkanExtentions, nullptr);
+
+	std::cout << nVulkanExtentions << " Vulkan extensions supported" << std::endl;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
