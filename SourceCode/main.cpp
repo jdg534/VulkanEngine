945d24c3e0ee40bf4c6e28d49ef8290a5d89f3b2
Josh Gibson <josh.gibson@reflexgaming.com>
2019-07-16 13:30:52 +0100

    Further tutorial progress, pick up from validation layers tutorial section Debugging instance creation and destruction

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -44,6 +44,7 @@ private:
 		catch (const std::exception& ex)
 		{
 			// if WIN32 OutputDebugString(), figure out Unix like equivelent
+
 		}
 	}
 	void InitWindow()
@@ -57,6 +58,7 @@ private:
 	void InitVulkan()
 	{
 		CreateVulkanInstance();
+
 		QueryVulkanExtentions();
 	}
 
@@ -77,7 +79,21 @@ private:
 		return validationLayerFound;
 	}
 
+	std::vector<const char*> GetRequiredVulkanExtentions()
+	{
+		// Message callback
+		uint32_t glfwRequiredExtCount = 0;
+		const char** glfwExtCStrs;
+		glfwExtCStrs = glfwGetRequiredInstanceExtensions(&glfwRequiredExtCount);
+		
+		std::vector<const char*> extCStrs(glfwExtCStrs, glfwExtCStrs + glfwRequiredExtCount);
 
+		if (AreVulkanValidationLayersSupported())
+		{
+			extCStrs.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+		}
+		return extCStrs;
+	}
 
 	void CreateVulkanInstance()
 	{
@@ -92,17 +108,16 @@ private:
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "Learning Vulkan Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+		appInfo.apiVersion = VK_API_VERSION_1_1;
 
 		VkInstanceCreateInfo instanceCreateInfo = {};
 		instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		instanceCreateInfo.pApplicationInfo = &appInfo; // tutorial used stack memory, should be ok
 		
-		uint32_t glfwRequiredExtCount = 0;
-		const char** glfwExtCStrs;
-		glfwExtCStrs = glfwGetRequiredInstanceExtensions(&glfwRequiredExtCount);
-		instanceCreateInfo.enabledExtensionCount = glfwRequiredExtCount;
-		instanceCreateInfo.ppEnabledExtensionNames = glfwExtCStrs;
+		const std::vector<const char*> requiredExtentions = GetRequiredVulkanExtentions();
+		
+		instanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtentions.size());
+		instanceCreateInfo.ppEnabledExtensionNames = requiredExtentions.data();
 
 		if (m_useVulkanValidationLayers)
 		{
@@ -124,6 +139,24 @@ private:
 			throw std::runtime_error("failed to create Vulkan instance");
 		}
 		std::cout << "Vulkan Instance created successfully" << std::endl;
+	}
+
+	void SetupVulkanDebugMessenger()
+	{
+		if (!m_useVulkanValidationLayers)
+			return;
+
+		VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		createInfo.pfnUserCallback = VulkanDebugCallback;
+		createInfo.pUserData = nullptr; // Optional
+
+		if (CreateDebugUtilsMessengerEXT(m_vulkanInstance, &createInfo, nullptr, &m_vulkanDebugMessenger) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to set up a Vulkan debug messenger!");
+		}
 	}
 
 	void QueryVulkanExtentions()
@@ -149,14 +182,55 @@ private:
 	}
 	void Shutdown()
 	{
+		if (m_useVulkanValidationLayers)
+		{
+			DestroyDebugUtilsMessengerEXT(m_vulkanInstance, m_vulkanDebugMessenger, nullptr);
+		}
 		vkDestroyInstance(m_vulkanInstance, nullptr);
 		glfwDestroyWindow(m_window);
 		glfwTerminate();
 		m_window = nullptr;
 	}
 
+	// Debug functions
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+		auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+		if (func != nullptr) {
+			return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+		}
+		else {
+			return VK_ERROR_EXTENSION_NOT_PRESENT;
+		}
+	}
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL VulkanDebugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) 
+	{
+		if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+		{
+			// std::cout << ?
+		}
+	    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+	    return VK_FALSE;
+	}
+
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) 
+	{
+		auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+		if (func != nullptr)
+		{
+			func(instance, debugMessenger, pAllocator);
+		}
+	}
+	// end of debug functions
+
 	GLFWwindow* m_window;
 	VkInstance m_vulkanInstance;
+	VkDebugUtilsMessengerEXT m_vulkanDebugMessenger;
 	const bool m_useVulkanValidationLayers;
 };
 
